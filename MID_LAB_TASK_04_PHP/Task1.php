4e100c200ad5b337917a353cac3c4e1c3138a43b
Shanzida101 <sharna0101@gmail.com>
2022-02-08 13:55:29 +0300

    1 2 3 4 done

---
+++ MID_LAB_TASK_04_PHP/Task1.php
@@ -0,0 +1,8 @@
+<?php
+  $lenght = 6;
+  $width = 7;
+  
+  echo " AREA OF RECTANGLE : " .$lenght*$width."\n"."</br>" ;
+  echo " PERIMETER OF RECTANGLE : "  . 2*($lenght*$width) ;
+  
+ ?>
---
+++ MID_LAB_TASK_04_PHP/Task2.php
@@ -0,0 +1,7 @@
+<?php
+  $amount = 2000 ;
+  
+  echo "Amount = " . $amount. "<br>";
+  echo "Vat =  " . $amount+($amount*0.15) ; 
+  
+ ?>
